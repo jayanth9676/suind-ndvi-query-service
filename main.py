a1ac3307309c825d412a7b8bf3cbcf34a8aa66c8
THALLAPELLI JAYANTH <jayanthgoud9676@gmail.com>
2024-07-27 21:53:38 +0530

    Udpated frontend UI and Linked Backend

--- main.py
+++ main.py
@@ -22,60 +22,8 @@ class QueryData(BaseModel):
 
 @app.get("/", response_class=HTMLResponse)
 async def root():
-    html_content = """
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>NDVI Query Service</title>
-        <style>
-            body {
-                font-family: Arial, sans-serif;
-                background-color: #f4f4f9;
-                margin: 0;
-                padding: 0;
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                height: 100vh;
-                text-align: center;
-            }
-            .container {
-                background-color: #fff;
-                padding: 20px;
-                border-radius: 8px;
-                box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-            }
-            h1 {
-                margin-bottom: 20px;
-                color: #333;
-            }
-            button {
-                padding: 10px 20px;
-                font-size: 16px;
-                color: #fff;
-                background-color: #007bff;
-                border: none;
-                border-radius: 5px;
-                cursor: pointer;
-                transition: background-color 0.3s;
-            }
-            button:hover {
-                background-color: #0056b3;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <h1>Welcome to the SUIND NDVI Query Service</h1>
-            <a href="/frontend/index.html">
-                <button>Go to NDVI Query Form</button>
-            </a>
-        </div>
-    </body>
-    </html>
-    """
+    with open("frontend/home.html") as file:
+        html_content = file.read()
     return HTMLResponse(content=html_content)
 
 # Define the POST endpoint /query_ndvi
